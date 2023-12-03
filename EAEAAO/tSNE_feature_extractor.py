fdef640a2751c964d5062f3e54ea3093006790b4
Fabian Plum <fabianplum@web.de>
2023-12-03 21:56:09 +0000

    Updated exception handling of feature extractor

--- EAEAAO/tSNE_feature_extractor.py
+++ EAEAAO/tSNE_feature_extractor.py
@@ -245,176 +245,183 @@ if __name__ == "__main__":
 
     for id, pose, size in tqdm(zip(order, all_poses_sorted, all_sizes_sorted), total=(len(order))):
         track_id += 1
-        id_val = int(id.split("_")[-1][:-4])
-        if stop_early:
-            if track_id == 100:
-                break
-        for instances in range(math.floor(int(len(pose) / clip_length))):
-            skip_instance = False
-
-            num_features = pose[int(instances * clip_length) + 1:int(instances * clip_length) + 1 + clip_length, 1:]
-            num_features = int(num_features.shape[1] / 3)
-
-            # drop_thresh_idx = [i*3 + 2 for i in range(num_features)]
-
-            frame_start = int(pose[int(instances * clip_length) + 1, 0])
-            # now create feature vector from instance
-            features = pose[int(instances * clip_length) + 1:int(instances * clip_length) + 1 + clip_length, 1:]
-
-            out_features = np.zeros(num_features * 2 * clip_length)
-            out_features_angles = np.zeros(9 * clip_length)  # abdomen, legs, antennae
-
-            loc_counter = 0
-
-            for frame in range(clip_length):
-                if display:
-                    blank_image = np.zeros((300, 300, 3), np.uint8)
-
-                try:
-                    avg_front = np.nanmean([features[frame][0:2], features[frame][126:128]], axis=0)
-                    avg_back = np.nanmean(
-                        [features[frame][3:5], features[frame][6:8], features[frame][9:11], features[frame][12:14]],
-                        axis=0)
-                    body_orientation = np.arccos(np.dot([0, 1], avg_back - avg_front) /
-                                                 (np.linalg.norm([0, 1]) * np.linalg.norm(avg_back - avg_front)))
-                except IndexError:
-                    skip_instance = True
+        try:
+            id_val = int(id.split("_")[-1][:-4])
+            if stop_early:
+                if track_id == 100:
                     break
+            for instances in range(math.floor(int(len(pose) / clip_length))):
+                skip_instance = False
 
-                for p in range(num_features):
-                    if features[frame][p * 3 + 2] >= 0.5:
-                        point = features[frame][p * 3:p * 3 + 2]
+                num_features = pose[int(instances * clip_length) + 1:int(instances * clip_length) + 1 + clip_length, 1:]
+                num_features = int(num_features.shape[1] / 3)
 
+                # drop_thresh_idx = [i*3 + 2 for i in range(num_features)]
+
+                frame_start = int(pose[int(instances * clip_length) + 1, 0])
+                # now create feature vector from instance
+                features = pose[int(instances * clip_length) + 1:int(instances * clip_length) + 1 + clip_length, 1:]
+
+                out_features = np.zeros(num_features * 2 * clip_length)
+                out_features_angles = np.zeros(9 * clip_length)  # abdomen, legs, antennae
+
+                loc_counter = 0
+
+                for frame in range(clip_length):
+                    if display:
+                        blank_image = np.zeros((300, 300, 3), np.uint8)
+
+                    try:
+                        avg_front = np.nanmean([features[frame][0:2], features[frame][126:128]], axis=0)
+                        avg_back = np.nanmean(
+                            [features[frame][3:5], features[frame][6:8], features[frame][9:11], features[frame][12:14]],
+                            axis=0)
+                        body_orientation = np.arccos(np.dot([0, 1], avg_back - avg_front) /
+                                                     (np.linalg.norm([0, 1]) * np.linalg.norm(avg_back - avg_front)))
+                    except IndexError:
+                        skip_instance = True
+                        break
+
+                    for p in range(num_features):
+                        if features[frame][p * 3 + 2] >= 0.5:
+                            point = features[frame][p * 3:p * 3 + 2]
+
+                            if centre_poses:
+                                point = point - avg_front + [150, 120]
+
+                            if align_poses:
+                                point = rotate(point, [150, 120], degrees=body_orientation * 180 / np.pi)
+
+                            # print(loc_counter)
+                            out_features[loc_counter:loc_counter + 2] = point
+
+                            if display:
+                                blank_image = cv2.circle(blank_image, (int(point[0]), int(point[1])),
+                                                         3,
+                                                         (int(255 * p / num_features),
+                                                          int(255 - 255 * p / num_features), 200),
+                                                         -1)
+                        loc_counter += 2
+
+                    # get joint angles and use the weighted mean of each leg for more robust estimates
+                    # the following is hard coded to get locations of key body parts but that's okay for now.
+                    # It works for insects.
+                    loc_antennae_l = np.reshape(features[frame][138:147], (-1, 3))
+                    loc_antennae_r = np.reshape(features[frame][129:138], (-1, 3))
+                    loc_leg_l_1 = np.reshape(features[frame][72:90], (-1, 3))
+                    loc_leg_l_2 = np.reshape(features[frame][90:108], (-1, 3))
+                    loc_leg_l_3 = np.reshape(features[frame][108:126], (-1, 3))
+                    loc_leg_r_1 = np.reshape(features[frame][18:36], (-1, 3))
+                    loc_leg_r_2 = np.reshape(features[frame][36:54], (-1, 3))
+                    loc_leg_r_3 = np.reshape(features[frame][54:72], (-1, 3))
+                    loc_abdomen = np.reshape(features[frame][3:18], (-1, 3))
+
+                    avg_loc_antennae_l = np.average(loc_antennae_l[:, :2], axis=0,
+                                                    weights=np.transpose([loc_antennae_l[:, 2], loc_antennae_l[:, 2]]))
+                    avg_loc_antennae_r = np.average(loc_antennae_r[:, :2], axis=0,
+                                                    weights=np.transpose([loc_antennae_r[:, 2], loc_antennae_r[:, 2]]))
+                    avg_loc_leg_l_1 = np.average(loc_leg_l_1[:, :2], axis=0,
+                                                 weights=np.transpose([loc_leg_l_1[:, 2], loc_leg_l_1[:, 2]]))
+                    avg_loc_leg_l_2 = np.average(loc_leg_l_2[:, :2], axis=0,
+                                                 weights=np.transpose([loc_leg_l_2[:, 2], loc_leg_l_2[:, 2]]))
+                    avg_loc_leg_l_3 = np.average(loc_leg_l_3[:, :2], axis=0,
+                                                 weights=np.transpose([loc_leg_l_3[:, 2], loc_leg_l_3[:, 2]]))
+                    avg_loc_leg_r_1 = np.average(loc_leg_r_1[:, :2], axis=0,
+                                                 weights=np.transpose([loc_leg_r_1[:, 2], loc_leg_r_1[:, 2]]))
+                    avg_loc_leg_r_2 = np.average(loc_leg_r_2[:, :2], axis=0,
+                                                 weights=np.transpose([loc_leg_r_2[:, 2], loc_leg_r_2[:, 2]]))
+                    avg_loc_leg_r_3 = np.average(loc_leg_r_3[:, :2], axis=0,
+                                                 weights=np.transpose([loc_leg_r_3[:, 2], loc_leg_r_3[:, 2]]))
+                    avg_loc_abdomen = np.average(loc_abdomen[:, :2], axis=0,
+                                                 weights=np.transpose([loc_abdomen[:, 2], loc_abdomen[:, 2]]))
+
+                    combined_poses = [[loc_antennae_l, avg_loc_antennae_l],
+                                      [loc_antennae_r, avg_loc_antennae_r],
+                                      [loc_leg_l_1, avg_loc_leg_l_1],
+                                      [loc_leg_l_2, avg_loc_leg_l_2],
+                                      [loc_leg_l_3, avg_loc_leg_l_3],
+                                      [loc_leg_r_1, avg_loc_leg_r_1],
+                                      [loc_leg_r_2, avg_loc_leg_r_2],
+                                      [loc_leg_r_3, avg_loc_leg_r_3],
+                                      [loc_abdomen, avg_loc_abdomen]]
+
+                    unit_vector_body_axis = [0, 1]
+
+                    for c, comb in enumerate(combined_poses):
+                        point = [comb[1][0], comb[1][1]]
                         if centre_poses:
                             point = point - avg_front + [150, 120]
 
                         if align_poses:
                             point = rotate(point, [150, 120], degrees=body_orientation * 180 / np.pi)
 
-                        # print(loc_counter)
-                        out_features[loc_counter:loc_counter + 2] = point
+                        bone_vector = [comb[1][0] - comb[0][0][0], comb[1][1] - comb[0][0][1]]
+                        unit_vector_bone_vector = bone_vector / np.linalg.norm(bone_vector)
+                        dot_product = np.dot(unit_vector_body_axis, unit_vector_bone_vector)
+                        out_features_angles[frame * len(combined_poses) + c] = (180 / np.pi) * np.arccos(
+                            np.clip(dot_product, -1.0, 1.0))
 
                         if display:
                             blank_image = cv2.circle(blank_image, (int(point[0]), int(point[1])),
-                                                     3,
-                                                     (int(255 * p / num_features),
-                                                      int(255 - 255 * p / num_features), 200),
+                                                     5,
+                                                     (255, 255, 255),
                                                      -1)
-                    loc_counter += 2
-
-                # get joint angles and use the weighted mean of each leg for more robust estimates
-                # the following is hard coded to get locations of key body parts but that's okay for now.
-                # It works for insects.
-                loc_antennae_l = np.reshape(features[frame][138:147], (-1, 3))
-                loc_antennae_r = np.reshape(features[frame][129:138], (-1, 3))
-                loc_leg_l_1 = np.reshape(features[frame][72:90], (-1, 3))
-                loc_leg_l_2 = np.reshape(features[frame][90:108], (-1, 3))
-                loc_leg_l_3 = np.reshape(features[frame][108:126], (-1, 3))
-                loc_leg_r_1 = np.reshape(features[frame][18:36], (-1, 3))
-                loc_leg_r_2 = np.reshape(features[frame][36:54], (-1, 3))
-                loc_leg_r_3 = np.reshape(features[frame][54:72], (-1, 3))
-                loc_abdomen = np.reshape(features[frame][3:18], (-1, 3))
-
-                avg_loc_antennae_l = np.average(loc_antennae_l[:, :2], axis=0,
-                                                weights=np.transpose([loc_antennae_l[:, 2], loc_antennae_l[:, 2]]))
-                avg_loc_antennae_r = np.average(loc_antennae_r[:, :2], axis=0,
-                                                weights=np.transpose([loc_antennae_r[:, 2], loc_antennae_r[:, 2]]))
-                avg_loc_leg_l_1 = np.average(loc_leg_l_1[:, :2], axis=0,
-                                             weights=np.transpose([loc_leg_l_1[:, 2], loc_leg_l_1[:, 2]]))
-                avg_loc_leg_l_2 = np.average(loc_leg_l_2[:, :2], axis=0,
-                                             weights=np.transpose([loc_leg_l_2[:, 2], loc_leg_l_2[:, 2]]))
-                avg_loc_leg_l_3 = np.average(loc_leg_l_3[:, :2], axis=0,
-                                             weights=np.transpose([loc_leg_l_3[:, 2], loc_leg_l_3[:, 2]]))
-                avg_loc_leg_r_1 = np.average(loc_leg_r_1[:, :2], axis=0,
-                                             weights=np.transpose([loc_leg_r_1[:, 2], loc_leg_r_1[:, 2]]))
-                avg_loc_leg_r_2 = np.average(loc_leg_r_2[:, :2], axis=0,
-                                             weights=np.transpose([loc_leg_r_2[:, 2], loc_leg_r_2[:, 2]]))
-                avg_loc_leg_r_3 = np.average(loc_leg_r_3[:, :2], axis=0,
-                                             weights=np.transpose([loc_leg_r_3[:, 2], loc_leg_r_3[:, 2]]))
-                avg_loc_abdomen = np.average(loc_abdomen[:, :2], axis=0,
-                                             weights=np.transpose([loc_abdomen[:, 2], loc_abdomen[:, 2]]))
-
-                combined_poses = [[loc_antennae_l, avg_loc_antennae_l],
-                                  [loc_antennae_r, avg_loc_antennae_r],
-                                  [loc_leg_l_1, avg_loc_leg_l_1],
-                                  [loc_leg_l_2, avg_loc_leg_l_2],
-                                  [loc_leg_l_3, avg_loc_leg_l_3],
-                                  [loc_leg_r_1, avg_loc_leg_r_1],
-                                  [loc_leg_r_2, avg_loc_leg_r_2],
-                                  [loc_leg_r_3, avg_loc_leg_r_3],
-                                  [loc_abdomen, avg_loc_abdomen]]
-
-                unit_vector_body_axis = [0, 1]
-
-                for c, comb in enumerate(combined_poses):
-                    point = [comb[1][0], comb[1][1]]
-                    if centre_poses:
-                        point = point - avg_front + [150, 120]
-
-                    if align_poses:
-                        point = rotate(point, [150, 120], degrees=body_orientation * 180 / np.pi)
-
-                    bone_vector = [comb[1][0] - comb[0][0][0], comb[1][1] - comb[0][0][1]]
-                    unit_vector_bone_vector = bone_vector / np.linalg.norm(bone_vector)
-                    dot_product = np.dot(unit_vector_body_axis, unit_vector_bone_vector)
-                    out_features_angles[frame * len(combined_poses) + c] = (180 / np.pi) * np.arccos(
-                        np.clip(dot_product, -1.0, 1.0))
 
                     if display:
-                        blank_image = cv2.circle(blank_image, (int(point[0]), int(point[1])),
-                                                 5,
-                                                 (255, 255, 255),
-                                                 -1)
+                        cv2.imshow("test", blank_image)
+                        cv2.waitKey(1)
 
-                if display:
-                    cv2.imshow("test", blank_image)
-                    cv2.waitKey(1)
+                # get std for each point, if avg std is too high, discard, use weight means
+                check_features = out_features.reshape(clip_length, num_features, 2)
+                check_features_std = np.std(check_features, axis=0)
+                mean_std = np.nanmean(check_features_std)
 
-            # get std for each point, if avg std is too high, discard, use weight means
-            check_features = out_features.reshape(clip_length, num_features, 2)
-            check_features_std = np.std(check_features, axis=0)
-            mean_std = np.nanmean(check_features_std)
+                if mean_std >= 45:
+                    skip_instance = True
 
-            if mean_std >= 45:
-                skip_instance = True
+                if skip_instance:
+                    continue
 
-            if skip_instance:
-                continue
+                if compute_size_class_for_each_action:
+                    x_diff = features[:, 0] - features[:, 15]  # b_tX -  b_a_5X
+                    y_diff = features[:, 1] - features[:, 16]  # b_tY -  b_a_5Y
 
-            if compute_size_class_for_each_action:
-                x_diff = features[:, 0] - features[:, 15]  # b_tX -  b_a_5X
-                y_diff = features[:, 1] - features[:, 16]  # b_tY -  b_a_5Y
+                    lengths = np.sqrt(np.square(x_diff) + np.square(y_diff))
+                    median_length = np.round(np.median(lengths) / px_to_mm_POSE, 2)
 
-                lengths = np.sqrt(np.square(x_diff) + np.square(y_diff))
-                median_length = np.round(np.median(lengths) / px_to_mm_POSE, 2)
+                    # now, use the appropriate class ID
+                    size = find_class([3, 4, 5, 6, 7], median_length)
 
-                # now, use the appropriate class ID
-                size = find_class([3, 4, 5, 6, 7], median_length)
+                # get speed from assocaited tracks
+                temp_tracks = tracks[frame_start:frame_start + clip_length, track_id * 2 + 1: track_id * 2 + 3]
+                temp_track_frames = np.arange(clip_length)
+                temp_tracks = np.stack((temp_track_frames, temp_tracks[:, 0], temp_tracks[:, 1]), axis=1)
+                try:
+                    tracks_MA = apply_moving_average(temp_tracks, 5)
+                except ValueError:
+                    continue
+                tracks_speed = get_derivative(tracks_MA, time_step=1 / 25)
+                speed = max(np.nanmean(tracks_speed[np.nonzero(tracks_speed)], axis=0), 0.001) / px_to_mm_TRACK
 
-            # get speed from assocaited tracks
-            temp_tracks = tracks[frame_start:frame_start + clip_length, track_id * 2 + 1: track_id * 2 + 3]
-            temp_track_frames = np.arange(clip_length)
-            temp_tracks = np.stack((temp_track_frames, temp_tracks[:, 0], temp_tracks[:, 1]), axis=1)
-            tracks_MA = apply_moving_average(temp_tracks, 5)
-            tracks_speed = get_derivative(tracks_MA, time_step=1 / 25)
-            speed = max(np.nanmean(tracks_speed[np.nonzero(tracks_speed)], axis=0), 0.001) / px_to_mm_TRACK
+                # get angular rates from computed angles
+                temp_angle_frames = np.arange(clip_length)
+                temp_angles = np.column_stack((temp_angle_frames, out_features_angles.reshape((clip_length, 9))))
+                temp_angles_MA = apply_moving_average(temp_angles, 5)
+                angular_velocity = get_derivative_angles(temp_angles_MA, time_step=1 / 25)
+                angular_velocity = angular_velocity.flatten()
 
-            # get angular rates from computed angles
-            temp_angle_frames = np.arange(clip_length)
-            temp_angles = np.column_stack((temp_angle_frames, out_features_angles.reshape((clip_length, 9))))
-            temp_angles_MA = apply_moving_average(temp_angles, 5)
-            angular_velocity = get_derivative_angles(temp_angles_MA, time_step=1 / 25)
-            angular_velocity = angular_velocity.flatten()
+                # create list of all features, starting with static features
+                temp_list = [os.path.basename(input_poses), material, frame_start, id_val, speed, size]
 
-            # create list of all features, starting with static features
-            temp_list = [os.path.basename(input_poses), material, frame_start, id_val, speed, size]
+                # and extending the feature vector with dynamic features (pose, angles, angular rates)
+                temp_list.extend(out_features.tolist())
+                temp_list.extend(out_features_angles.tolist())
+                temp_list.extend(angular_velocity.tolist())
 
-            # and extending the feature vector with dynamic features (pose, angles, angular rates)
-            temp_list.extend(out_features.tolist())
-            temp_list.extend(out_features_angles.tolist())
-            temp_list.extend(angular_velocity.tolist())
+                out_list.append(temp_list)
 
-            out_list.append(temp_list)
+        except:
+            continue
 
     cv2.destroyAllWindows()
 
