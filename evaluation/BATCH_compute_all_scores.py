0ef90c5ff278cd9bc7f9c6049120cd7c5b02b1f4
Fabian Plum <fabianplum@web.de>
2024-02-23 13:31:53 +0000

    Fixed issues relating to missing function outputs when multi-threading

--- evaluation/BATCH_compute_all_scores.py
+++ evaluation/BATCH_compute_all_scores.py
@@ -1,26 +1,30 @@
 import os
-import concurrent
+from concurrent.futures import ProcessPoolExecutor as Executor
 import compute_all_scores
+import pandas as pd
 
 
 def worker(task):
     # task = [model, requires_gt_extraction, detection]
     try:
         if not task[2]:
-            compute_all_scores.compute_scores(input_file=task[0],
-                                              output=task[3])
+            return compute_all_scores.compute_scores(input_file=task[0],
+                                                     output=task[3],
+                                                     dataset_name=task[4])
 
         else:
             if task[1]:
-                compute_all_scores.compute_scores(input_file=task[0],
-                                                  output=task[3],
-                                                  detection_format=True,
-                                                  gt_from_name=True)
+                return compute_all_scores.compute_scores(input_file=task[0],
+                                                         output=task[3],
+                                                         detection_format=True,
+                                                         gt_from_name=True,
+                                                         dataset_name=task[4])
 
             else:
-                compute_all_scores.compute_scores(input_file=task[0],
-                                                  output=task[3],
-                                                  detection_format=True)
+                return compute_all_scores.compute_scores(input_file=task[0],
+                                                         output=task[3],
+                                                         detection_format=True,
+                                                         dataset_name=task[4])
     except Exception as error:
         # handle the exception
         print("An exception occurred:", error)
@@ -32,28 +36,80 @@ if __name__ == '__main__':
 
     # [path, special-weight-formatting, detection]
 
-    all_file_paths = [["D:/WOLO/HPC_trained_models/WOLO_DETECT/RESULTS", False, True],
-                      ["D:/WOLO/HPC_trained_models/WOLO_DETECT/RESULTS_TEST_128x128_DSLR_C920", False, True],
-                      ["D:/WOLO/HPC_trained_models/WOLO_DETECT/RESULTS_TEST_128x128_CORVIN9000", True, True],
-                      ["D:/WOLO/HPC_trained_models/WOLO/TRAINED_MODELS", False, False],
-                      ["D:/WOLO/BENCHMARK/RESULTS_TEST/CORVIN9000/predictions", False, False],
-                      ["D:/WOLO/BENCHMARK/RESULTS_TEST/DSLR_C920/predictions", False, False]]
+    all_file_paths = [["D:/WOLO/HPC_trained_models/WOLO/TRAINED_MODELS", False, False,
+                       "VAL"],
+                      ["D:/WOLO/BENCHMARK/RESULTS_TEST/CORVIN9000/predictions", False, False,
+                       "CORVIN9000"],
+                      ["D:/WOLO/BENCHMARK/RESULTS_TEST/DSLR_C920/predictions", False, False,
+                       "DSLR-C920"],
+                      ["D:/WOLO/HPC_trained_models/WOLO_DETECT/RESULTS", False, True,
+                       "VAL"],
+                      ["D:/WOLO/HPC_trained_models/WOLO_DETECT/RESULTS_TEST_128x128_DSLR_C920", False, True,
+                       "DSLR-C920"],
+                      ["D:/WOLO/HPC_trained_models/WOLO_DETECT/RESULTS_TEST_128x128_CORVIN9000", True, True,
+                       "CORVIN9000"]]
+    """
+
+    all_file_paths = [["D:/WOLO/HPC_trained_models/WOLO_DETECT/RESULTS", False, True,
+                       "VAL"]]
+    """
+
+    parallel = True
 
     # define pool_size and create worker pool
-    executor = concurrent.futures.ProcessPoolExecutor(10)
-    futures = []
+    all_results = {}
 
-    for eval_type in all_file_paths:
-        model_path = eval_type[0]
+    if parallel:
+        with Executor() as executor:
 
-        model_list = os.listdir(model_path)
+            for e, eval_type in enumerate(all_file_paths):
+                print("SET", e, "of", eval_type)
 
-        for model in model_list:
-            print(model)
-            futures.append(
-                executor.submit(worker,
-                                [str(os.path.join(model_path, model)), eval_type[1], eval_type[2], results_path]))
+                model_path = eval_type[0]
 
-    concurrent.futures.wait(futures)
+                model_list = os.listdir(model_path)
+
+                for model in model_list:
+
+                    all_results[model + "_data_" + eval_type[3]] = executor.submit(worker,
+                                                                                   [str(os.path.join(model_path,
+                                                                                                     model)),
+                                                                                    eval_type[1],
+                                                                                    eval_type[2],
+                                                                                    results_path,
+                                                                                    eval_type[3]])
+
+        valid_results = 0
+        for i, future in all_results.items():
+            if future.result() is not None:
+                print(i, "  :  ", future.result())
+                valid_results += 1
+
+        print("Valid results:", valid_results)
+
+    else:
+        for e, eval_type in enumerate(all_file_paths):
+            print("SET", e, "of", eval_type)
+            model_path = eval_type[0]
+
+            model_list = os.listdir(model_path)
+
+            for model in model_list:
+
+                all_results[model + "_data_" + eval_type[3]] = worker([str(os.path.join(model_path, model)),
+                                                                       eval_type[1],
+                                                                       eval_type[2],
+                                                                       results_path,
+                                                                       eval_type[3]])
+
+        print("\n\n================================================\n\n")
+
+        valid_results = 0
+        for i, result in all_results.items():
+            if result is not None:
+                print(i, "  :  ", result)
+                valid_results += 1
+
+        print("Valid results:", valid_results)
 
     print("\n\n================================================\n\n FINISHED")
