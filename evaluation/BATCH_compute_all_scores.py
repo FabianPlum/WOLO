e09a4c73eacb01f6310b70225a93479f8fd43b10
Fabian Plum <fabianplum@web.de>
2024-02-25 15:50:53 +0000

    Added generation of comprehensive results table with all metrics

--- evaluation/BATCH_compute_all_scores.py
+++ evaluation/BATCH_compute_all_scores.py
@@ -1,7 +1,7 @@
 import os
+import csv
 from concurrent.futures import ProcessPoolExecutor as Executor
 import compute_all_scores
-import pandas as pd
 
 
 def worker(task):
@@ -52,12 +52,12 @@ if __name__ == '__main__':
 
     all_file_paths = [["D:/WOLO/HPC_trained_models/WOLO_DETECT/RESULTS", False, True,
                        "VAL"]]
+    
     """
-
     parallel = True
 
     # define pool_size and create worker pool
-    all_results = {}
+    all_results = []
 
     if parallel:
         with Executor() as executor:
@@ -70,20 +70,20 @@ if __name__ == '__main__':
                 model_list = os.listdir(model_path)
 
                 for model in model_list:
-                    all_results[model + "_data_" + eval_type[3]] = executor.submit(worker,
-                                                                                   [str(os.path.join(model_path,
-                                                                                                     model)),
-                                                                                    eval_type[1],
-                                                                                    eval_type[2],
-                                                                                    results_path,
-                                                                                    eval_type[3]])
+                    all_results.append(executor.submit(worker,
+                                                       [str(os.path.join(model_path,
+                                                                         model)),
+                                                        eval_type[1],
+                                                        eval_type[2],
+                                                        results_path,
+                                                        eval_type[3]]))
 
-        out_results = all_results.copy()
-        for i, future in all_results.items():
-            if future.result() is not None:
-                print(i, "  :  ", future.result())
-            else:
-                del out_results[i]
+        out_results = []
+        for i, future in enumerate(all_results):
+            result = future.result()
+            if result is not None:
+                print(i, "  :  ", result)
+                out_results.append(result)
 
         print("Out results:", len(out_results))
 
@@ -96,21 +96,79 @@ if __name__ == '__main__':
             model_list = os.listdir(model_path)
 
             for model in model_list:
-                all_results[model + "_data_" + eval_type[3]] = worker([str(os.path.join(model_path, model)),
-                                                                       eval_type[1],
-                                                                       eval_type[2],
-                                                                       results_path,
-                                                                       eval_type[3]])
+                all_results.append(worker([str(os.path.join(model_path, model)),
+                                           eval_type[1],
+                                           eval_type[2],
+                                           results_path,
+                                           eval_type[3]]))
 
         print("\n\n================================================\n\n")
 
-        out_results = all_results.copy()
-        for i, result in all_results.items():
+        out_results = []
+        for i, result in enumerate(all_results):
             if result is not None:
                 print(i, "  :  ", result)
-            else:
-                del out_results[i]
+                out_results.append(result)
 
         print("Out results:", len(out_results))
+
+    """
+    Now, sort the retrieved results into dicts to dump into a csv file:
+                                                ________________________(test) dataset_______________________________
+                attributes                      VAL                     DSLR-C920               CORVIN9000
+    model       real_data   synt_data   ...     R^2     MAPE    COV     R^2     MAPE    COV     R^2     MAPE    COV
+    model_XYZ   ####        ####        ...     ####    ####    ####    ####    ####    ####    ####    ####    #### 
+    
+    all attributes
+    "model"
+    "dataset"           -> VAL / DSLR-C920 / CORVIN9000
+    "inference_type"    -> REG / CLASS / DETECT
+    "real data"         -> MultiCamAnts
+    "synth data"        -> simple / standard / all
+    "sigma"             -> one-hote / 0.5 / 1.0 / 2.0 / 4.0
+    "R^2" [log]  
+    "slope"
+    "y intercept"
+    "accuracy"
+    "MAPE_true"
+    "MAPE_ideal"
+    "MAPE_class"
+    "COV"
+    """
+
+    attributes = ["model", "dataset", "inference_type", "real data", "synth data", "sigma"]
+
+    out_clean = {}
+    for result in out_results:
+        temp_entry = {}
+        # if the model is already part of the dictionary, only add results
+        if result["model"] in out_clean:
+            for key, value in result.items():
+                if key not in attributes:
+                    out_clean[result["model"]][result["dataset"] + "_" + key] = value
+
+        # if the model appears for the first time, add a new entry
+        else:
+            # first, get all static attributes
+            for key in attributes:
+                # don't write out (test) dataset
+                if key != "dataset":
+                    temp_entry[key] = result[key]
+            # then, get all test-data specific results
+            for key, value in result.items():
+                if key not in attributes:
+                    temp_entry[result["dataset"] + "_" + key] = value
+
+            out_clean[result["model"]] = temp_entry
+
+        print(out_clean[result["model"]])
+
+    print(len(out_clean))
+    print(out_clean[list(out_clean.keys())[0]].keys())
+
+    with open('all_results.csv', 'w', newline='', encoding='utf-8') as f:
+        w = csv.DictWriter(f, out_clean[list(out_clean.keys())[0]].keys())
+        w.writeheader()
+        w.writerows(list(out_clean.values()))
 
     print("\n\n================================================\n\n FINISHED")
